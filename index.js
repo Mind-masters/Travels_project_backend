a43d04c5377d01c3a628251b3909c424cea66c91
williamdinh <hoangdinh2411@gmail.com>
2022-09-11 23:40:00 +0200

    add vercal.json file to deploy project on vercel

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(bodyParser.urlencoded({limit:'30mb', extended : true}));
 app.use(cors());
 
 app.use("/api/v1", require("./api/index"));
-
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 app.listen(PORT, () => {
   db();
   console.log("The server listening on port " + PORT);
