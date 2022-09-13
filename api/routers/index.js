4c8eab9beeccda3e3cdc78377944b88e607005ad
dominykas <bubnys.it@gmail.com>
2022-09-13 21:06:41 +0300

    added places routes, schema and controllers

--- api/routers/index.js
+++ api/routers/index.js
@@ -2,12 +2,6 @@ const middlewareOptions = require("../middleware/auth.middleware");
 module.exports = function (app) {
   // Admin APIs
 
-<<<<<<< HEAD
-  // Client 
-  app.use('/client', require('./client/auth.router'))
-  app.use('/user', require('./client/places.router'))
-
-=======
   app.use("/admin", middlewareOptions.admin);
 
   // User
@@ -18,5 +12,4 @@ module.exports = function (app) {
 
   app.use("/client", middlewareOptions.client);
   app.use("/client", require("./client/auth.routers"));
->>>>>>> 62b5d5873850179a1a237e98c5103135449e5530
 };
